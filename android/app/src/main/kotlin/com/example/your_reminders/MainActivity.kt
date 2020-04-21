677a6d008de570c4306934a711758251b9f9cde1
gaya3d2611 <d.gaya32611@gmail.com>
2020-04-21 14:12:45 +0530

    fcm added

--- android/app/src/main/kotlin/com/example/your_reminders/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/your_reminders/MainActivity.kt
@@ -8,5 +8,20 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    val msg = getString(R.string.msg_token_fmt, token)
+                    Log.d(TAG, msg)
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                })
     }
 }
--- lib/screens/reminder.dart
+++ lib/screens/reminder.dart
@@ -1,53 +1,149 @@
+import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'firstscreen.dart';
 import 'package:intl/intl.dart';
-
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:simpletime_picker/constant.dart';
 import 'package:simpletime_picker/scrollable_time_picker.dart';
 import 'package:simpletime_picker/time_picker.dart';
-
-
-
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 class remider extends StatefulWidget {
   @override
   _remiderState createState() => _remiderState();
 }
-
 class _remiderState extends State<remider> {
   final title= new TextEditingController();
   final body= new TextEditingController();
   final time= new TextEditingController();
   final reminderTime= TextEditingController();
-  TimeOfDay _time = TimeOfDay.now();
-
+   /*TimeOfDay _time = TimeOfDay.now();
   void onTimeChanged(TimeOfDay newTime) {
     setState(() {
       _time = newTime;
     });
+  } */
+  final FirebaseMessaging messaging =FirebaseMessaging();
+  @override
+  void initState(){
+    super.initState();
+    messaging.getToken().then((token){
+      print("token:" + token);
+    });
   }
   @override
   Widget build(BuildContext context) {
+   /* FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =  FlutterLocalNotificationsPlugin();
+    var initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification,);
+    Future<void> showWeeklyAtDayAndTime(Time time, Day day, int id, String title, String description) async {
+      final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'show weekly channel id',
+        'show weekly channel name',
+        'show weekly description',
+      );
+      final iOSPlatformChannelSpecifics = IOSNotificationDetails();
+      final platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics,
+        iOSPlatformChannelSpecifics,
+      );
+      await flutterLocalNotificationsPlugin.showWeeklyAtDayAndTime(
+        id,
+        title,
+        description,
+        day,
+        time,
+        platformChannelSpecifics,
+      );
+    }
+    Future<void> showDailyAtTime(Time time, int id, String title, String description) async {
+      final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'show weekly channel id',
+        'show weekly channel name',
+        'show weekly description',
+      );
+      final iOSPlatformChannelSpecifics = IOSNotificationDetails();
+      final platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics,
+        iOSPlatformChannelSpecifics,
+      );
+      await flutterLocalNotificationsPlugin.showDailyAtTime(
+        id,
+        title,
+        description,
+        time,
+        platformChannelSpecifics,
+      );
+    }
+    Future<List<PendingNotificationRequest>> getScheduledNotifications() async {
+      final pendingNotifications = await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+      return pendingNotifications;
+    } */
+
+    /*Future<void> cancelNotification(int id) async {
+      await flutterLocalNotificationsPlugin.cancel(id);
+    }
+
+    Future<void> cancelAllNotifications() async {
+      await flutterLocalNotificationsPlugin.cancelAll();
+    } */
+
+   //FirebaseMessaging firebaseMessaging= FirebaseMessaging();
+   /*@override initState(){
+     super.initState();
+     firebaseMessaging.configure(
+           onMessage: (Map<String, dynamic> message) async {
+             print("onMessage: $message");
+             final notification = message['notifications'];
+             //_showItemDialog(message);
+           },
+           onLaunch: (Map<String, dynamic> message) async {
+             print("onLaunch: $message");
+            // _navigateToItemDetail(message);
+           },
+           onResume: (Map<String, dynamic> message) async {
+             print("onResume: $message");
+            // _navigateToItemDetail(message);
+           },
+     );
+   }*/
     DateTime now = DateTime.now();
-    final format = DateFormat("HH:mm");
-    String formattedDate = DateFormat('kk:mm:ss').format(now);
+    String formattedDate = DateFormat('hh:mm:ss').format(now);
     return Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(onPressed: (){
+        floatingActionButton: FloatingActionButton(onPressed: ()async{
           time.text= formattedDate.toString();
+         /* await flutterLocalNotificationsPlugin.showWeeklyAtDayAndTime(id, title, body, day, notificationTime, notificationDetails)Time(
+            Time(1,30,0),
+            Day.Sunday,
+            0,
+            'first notification',
+            'description',
+          );
+          setState(() {
+            flutterLocalNotificationsPlugin.getScheduledNotifications();
+          }); */
           submit();
         },
           child: Icon(Icons.check),),
         appBar: AppBar(
           actions: <Widget>[
-            FlatButton(
+            /*FlatButton(
               onPressed: (){
                 FirebaseAuth.instance.signOut();
               },
               child: Icon(Icons.exit_to_app, color: Colors.white,),
-            )
+            ) */
           ],
           title: Text("Your Reminders", style: TextStyle(color: Colors.white),),
           centerTitle: true,
@@ -116,6 +212,8 @@ class _remiderState extends State<remider> {
                                 isTwelveHourFormat: true);
                             print("ini apa ? $result");
                             reminderTime.text= result.toString();
+                            //String reminderTimee = DateFormat('hh:mm:ss').format(result),
+                            
                           },
                           child: Text("Click to enter time"), textColor: Colors.white,),
                     ),
@@ -132,5 +230,11 @@ class _remiderState extends State<remider> {
         .setData({ 'title': title.text, 'body': body.text, 'time': time.text, 'reminderTime': reminderTime.text });
     Navigator.pop(context);
   }
+ /* Future onSelectNotification(String payload) async {
+    if (payload != null)
+      {
+        print('notification payload: ' + payload);
+      }
+  } */
 
 }
